5df28148b64f179acacfbd7a3135c20250692747
whitedevel572 <143584885+whitedevel572@users.noreply.github.com>
2023-12-14 09:30:48 +0530

    Update inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -56,7 +56,7 @@ pm_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="☁️ sᴏᴜʀᴄᴇ ☁️", url="https://github.com/AnonymousX1025/FallenMusic"
+            text="☁️ owner ☁️", url="https://t.me/ll_sanatani_ll"
         ),
         InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=config.OWNER_ID),
     ],
@@ -76,7 +76,7 @@ gp_buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="☁️ sᴏᴜʀᴄᴇ ☁️", url="https://github.com/TheAnonymous2005/FallenMusic"
+            text="☁️ owner ☁️", url="https://t.me/ll_sanatani_ll"
         ),
         InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=config.OWNER_ID),
     ],
